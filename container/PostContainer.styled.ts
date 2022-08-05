c6662ebb3cce9f8d03d6914ad6082cabf42d21e5
OH SW <ohseungo@naver.com>
2022-08-05 13:18:16 +0900

    feat : post 내 줄간격 및 font 크기 단위 조절

--- container/PostContainer.styled.ts
+++ container/PostContainer.styled.ts
@@ -2,15 +2,15 @@ import styled from "@emotion/styled";
 
 export const Root = styled.section`
   color: var(--sub-txt-color);
-  line-height: 165%;
+  line-height: 170%;
   h1 {
     line-height: 120%;
-    font-size: 36px;
+    font-size: 2.25rem;
     font-weight: 800;
     margin: 30px 0 12px;
   }
   h2 {
-    font-size: 28px;
+    font-size: 2rem;
     font-weight: 700;
     margin: 3.6rem 0 0;
   }
@@ -23,9 +23,11 @@ export const Root = styled.section`
     color: var(--main-txt-color);
   }
   p {
-    font-size: 18px;
+    font-size: 1.125rem;
   }
-
+  ul {
+    font-size: 1.125rem;
+  }
   img {
     max-width: 100%;
   }
