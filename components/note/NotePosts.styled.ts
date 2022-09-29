451db2289a2d263d7c89391f3ed26fca56a90ebb
OH SW <ohseungo@naver.com>
2022-09-29 21:42:12 +0900

    feat : tailwind preflight 으로 인한 변경 원복

--- components/note/NotePosts.styled.ts
+++ components/note/NotePosts.styled.ts
@@ -4,6 +4,23 @@ export const Root = styled.div``;
 
 export const Partition = styled.section`
   margin-bottom: 3rem;
+
+  h1 {
+    font-weight: bold;
+    font-size: 1.5em;
+    margin-block-start: 0.83em;
+    margin-block-end: 0.83em;
+  }
+
+  ol {
+    display: block;
+    list-style-type: decimal;
+    padding-inline-start: 40px;
+  }
+
+  li {
+    display: list-item;
+  }
 `;
 
 export const NoteLink = styled.li`
